afa7e304232fd2713a46a9ceadf9c91010420ca8
km-devlab <khrishamcse2020@gmail.com>
2026-05-20 23:44:25 +0530

    Fixed BASE_DIR order in settings

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
