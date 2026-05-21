d36ae4f47ab4ac949532dd469420dc93ebc19843
km-devlab <khrishamcse2020@gmail.com>
2026-05-21 22:21:58 +0530

    Use fallback image CDN

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -1,17 +1,17 @@
 #original code
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('users/', include('users.urls')),
-#     path('',include('users.urls')),
-#     path('movies/', include('movies.urls')),
-# ]
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('',include('users.urls')),
+    path('movies/', include('movies.urls')),
+]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # attempt 1
 # from django.contrib import admin
@@ -28,16 +28,16 @@
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #attempt 2
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
 
-    path('', include('movies.urls')),
-    path('', include('users.urls')),
-]
+#     path('', include('movies.urls')),
+#     path('', include('users.urls')),
+# ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
