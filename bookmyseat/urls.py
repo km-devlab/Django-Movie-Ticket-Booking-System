994f6c4facb366e9a524970425904868f59c994f
km-devlab <khrishamcse2020@gmail.com>
2026-05-21 21:16:53 +0530

    Fixed users URL routing

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -13,15 +13,31 @@
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# attempt 1
+# from django.contrib import admin
+# from django.urls import path, include
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('movies.urls')),
+# ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#attempt 2
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('movies.urls')),
+    path('', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
