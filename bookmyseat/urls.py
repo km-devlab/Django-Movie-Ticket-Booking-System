d9f45af3e9c299861780bd8d41dda5dff1b15f54
km-devlab <khrishamcse2020@gmail.com>
2026-06-02 01:35:05 +0530

    Fix duplicate urlpatterns override in main URLconf

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -3,23 +3,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from movies.views import emergency_database_reset
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('',include('users.urls')),
+    path('', include('users.urls')),
     path('movies/', include('movies.urls')),
+    
+    # Secret DB Reset Route
+    path('secret-db-reset/', emergency_database_reset, name='db_reset'),
 ]
 
+# Serve media files during development or if DEBUG is True
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.urls import path
-from movies.views import emergency_database_reset  # <-- Add this import
-
-urlpatterns = [
-    # ... your existing paths ...
-    path('secret-db-reset/', emergency_database_reset, name='db_reset'), # <-- Add this line
-]
 
 # attempt 1
 # from django.contrib import admin
