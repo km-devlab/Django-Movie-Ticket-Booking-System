fdb404aa7df23be049ab58cf2bd60a24adb0bbee
km-devlab <khrishamcse2020@gmail.com>
2026-06-02 02:04:46 +0530

    Restore UserRegisterForm alongside UserUpdateForm in forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+# 1. Your Existing User Registration Form (Restored)
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+
+# 2. Your New Profile Update Form
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
 
@@ -10,6 +26,5 @@ class UserUpdateForm(forms.ModelForm):
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Adds Bootstrap styles so the fields look nice inside your theme layout
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
